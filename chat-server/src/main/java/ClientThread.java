bbcda330144e43a4643a0e25b92c2f10a1fb7ece
victoria <viktoriya.zyk@mail.ru>
2022-05-19 22:59:11 +0300

    chat code

---
+++ chat-server/src/main/java/ClientThread.java
@@ -0,0 +1,44 @@
+import java.net.Socket;
+
+public class ClientThread extends SocketThread {
+    private String nickname;
+    private boolean isAuthorized;
+    private boolean isReconnecting;
+
+    public ClientThread(SocketThreadListener listener, String name, Socket socket) {
+        super(listener, name, socket);
+    }
+
+    public boolean isReconnecting() {
+        return isReconnecting;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public boolean isAuthorized() {
+        return isAuthorized;
+    }
+
+    void reconnect() {
+        isReconnecting = true;
+        close();
+    }
+
+    void authAccept(String nickname) {
+        isAuthorized = true;
+        this.nickname = nickname;
+        sendMessage(Messages.getAuthAccept(nickname));
+    }
+
+    void authFail() {
+        sendMessage(Messages.getAuthDenied());
+        close();
+    }
+
+    void msgFormatError(String msg) {
+        sendMessage(Messages.getMsgFormatError(msg));
+        close();
+    }
+}
