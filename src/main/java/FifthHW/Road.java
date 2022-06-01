84661558dad1ac15b92b43ea35c96b5036a49d2c
victoria <viktoriya.zyk@mail.ru>
2022-06-01 21:32:16 +0300

    homework lesson 5

---
+++ src/main/java/FifthHW/Road.java
@@ -0,0 +1,26 @@
+package FifthHW;
+
+import java.util.concurrent.BrokenBarrierException;
+
+public class Road extends Stage {
+
+    public Road(int length) {
+        this.length = length;
+        this.description = "Дорога " + length + " метров";
+    }
+
+    @Override
+    public void go(Car c) {
+        try {
+            System.out.println(c.getName() + " начал этап: " + description);
+            Thread.sleep(length / c.getSpeed() * 1000);
+            FifthHW.roadStage.await();
+            System.out.println(c.getName() + " закончил этап: " + description);
+            if (this.length == 40) {
+                FifthHW.finishLine.countDown();
+            }
+        } catch (InterruptedException | BrokenBarrierException e) {
+            e.printStackTrace();
+        }
+    }
+}
